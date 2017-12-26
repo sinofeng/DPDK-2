caa921a5c40a8ed9d36eb8082a1ea5630a18c0e5
yangjinlong <yangjinlong@dvt.dvt.com>
2017-12-26 18:38:28 +0800

    [PFRING-0]:高速数据收发平台开发 [问题性质]:新特性开发 [修改说明]： 1. 格式化代码 [验证]： 1. 不影响编译 Signed-off-by: Yangjinlong <yjl@github.com>

--- examples/echo/echo-client/echo_client.c
+++ examples/echo/echo-client/echo_client.c
@@ -29,59 +29,60 @@ static int num_cores;
 static int core_limit;
 
 int main (int argc, char *argv[]) {
-	struct mtcp_conf mcfg;
-	char *conf_file;	
+  struct mtcp_conf mcfg;
+  char *conf_file;  
   mctx_t mctx;
   int sockid;
   struct sockaddr_in addr;
   char buf[BUFFER_SIZE];
   int ret, o;
 
-	num_cores = GetNumCPUs();
-	core_limit = num_cores;    
+  num_cores = GetNumCPUs();
+  core_limit = num_cores;    
 
-	while (-1 != (o = getopt(argc, argv, "N:f:"))) {
-		switch(o) {
-		case 'N':
-			core_limit = mystrtol(optarg, 10);
-			if (core_limit > num_cores) {
-				on_error("CPU limit should be smaller than the "
-					     "number of CPUS: %d\n", num_cores);
-				return -1;
-			} else if (core_limit < 1) {
-				on_error("CPU limit should be greater than 0\n");
-				return -1;
-			}
-			/** 
-			 * it is important that core limit is set 
-			 * before mtcp_init() is called. You can
-			 * not set core_limit after mtcp_init()
-			 */
-			mtcp_getconf(&mcfg);
-			mcfg.num_cores = core_limit;
-			mtcp_setconf(&mcfg);
-			break;
-		case 'f':
-			conf_file = optarg;
-			break;
-		}
-	}	
+  while (-1 != (o = getopt(argc, argv, "N:f:"))) {
+    switch(o) {
+    case 'N':
+      core_limit = mystrtol(optarg, 10);
+      if (core_limit > num_cores) {
+        on_error("CPU limit should be smaller than the "
+               "number of CPUS: %d\n", num_cores);
+        return -1;
+      } else if (core_limit < 1) {
+        on_error("CPU limit should be greater than 0\n");
+        return -1;
+      }
+      /** 
+       * it is important that core limit is set 
+       * before mtcp_init() is called. You can
+       * not set core_limit after mtcp_init()
+       */
+      mtcp_getconf(&mcfg);
+      mcfg.num_cores = core_limit;
+      mtcp_setconf(&mcfg);
+      break;
+    case 'f':
+      conf_file = optarg;
+      break;
+    }
+  } 
 
-	if (conf_file == NULL) {
-		on_error("mTCP configuration file is not set!\n");
-		exit(-1);
-	}
-	
-	ret = mtcp_init(conf_file);
-	if (ret) {
-		on_error("Failed to initialize mtcp.\n");
-		exit(-1);
-	}
-	
+  if (conf_file == NULL) {
+    on_error("mTCP configuration file is not set!\n");
+    exit(-1);
+  }
+  
+  ret = mtcp_init(conf_file);
+  if (ret) {
+    on_error("Failed to initialize mtcp.\n");
+    exit(-1);
+  }
+  
   mtcp_core_affinitize(0);
   mctx = mtcp_create_context(0);
   sockid = mtcp_socket(mctx, AF_INET, SOCK_STREAM, 0);
-  if (sockid < 0) on_error("Could not create socket\n");
+  if (sockid < 0) 
+    on_error("Could not create socket\n");
 
   addr.sin_family = AF_INET;
   addr.sin_port = htons(ECHO_ECHO_PORT);
@@ -89,35 +90,36 @@ int main (int argc, char *argv[]) {
 
   printf("connect to %s:%d\n", ECHO_SERVER_IP, ECHO_ECHO_PORT);
 
-	ret = mtcp_connect(mctx, sockid, 
-			(struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+  ret = mtcp_connect(mctx, sockid, 
+      (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
 
-	if (ret < 0) {
-		if (errno != EINPROGRESS) {
-			perror("mtcp_connect failed");
-			mtcp_close(mctx, sockid);
-			return -1;
-		}
-	}
+  if (ret < 0) {
+    if (errno != EINPROGRESS) {
+      perror("mtcp_connect failed");
+      mtcp_close(mctx, sockid);
+      return -1;
+    }
+  }
  
-	ret = mtcp_setsock_nonblock(mctx, sockid);
-	if (ret < 0) {
-		on_error("Failed to set socket in nonblocking mode.\n");
-		exit(-1);
-	}
+  ret = mtcp_setsock_nonblock(mctx, sockid);
+  if (ret < 0) {
+    on_error("Failed to set socket in nonblocking mode.\n");
+    exit(-1);
+  }
  
   while (1) {
-	  ret = mtcp_write(mctx, sockid, "12312313", strlen("12312313"));
-	  if (ret < 0) on_error("Client write failed\n");
+    ret = mtcp_write(mctx, sockid, "12312313", strlen("12312313"));
+    if (ret < 0) 
+      on_error("Client write failed\n");
 
     while (1) {
       int read = mtcp_recv(mctx, sockid, buf, BUFFER_SIZE, 0);
-	  if(read == 0) {
-		  on_error("Client read failed\n");
-		  break;
-	  } else {
-		  continue;
-	  }
+      if(read == 0) {
+        on_error("Client read failed\n");
+        break;
+      } else {
+        continue;
+      }
     }
   }
 
--- examples/echo/echo-server/echo_server.c
+++ examples/echo/echo-server/echo_server.c
@@ -27,91 +27,98 @@ static int num_cores;
 static int core_limit;
 
 int main (int argc, char *argv[]) {
-	struct mtcp_conf mcfg;
-	char *conf_file;
-	
+  struct mtcp_conf mcfg;
+  char *conf_file;
+  
   mctx_t mctx;
   int server_fd, client_fd, err, o;
   struct sockaddr_in server, client;
   char buf[BUFFER_SIZE];
 
-	num_cores = GetNumCPUs();
-	core_limit = num_cores;  
+  num_cores = GetNumCPUs();
+  core_limit = num_cores;  
 
-	while (-1 != (o = getopt(argc, argv, "N:f:"))) {
-		switch(o) {
-		case 'N':
-			core_limit = mystrtol(optarg, 10);
-			if (core_limit > num_cores) {
-				on_error("CPU limit should be smaller than the "
-					     "number of CPUS: %d\n", num_cores);
-				return -1;
-			} else if (core_limit < 1) {
-				on_error("CPU limit should be greater than 0\n");
-				return -1;
-			}
-			/** 
-			 * it is important that core limit is set 
-			 * before mtcp_init() is called. You can
-			 * not set core_limit after mtcp_init()
-			 */
-			mtcp_getconf(&mcfg);
-			mcfg.num_cores = core_limit;
-			mtcp_setconf(&mcfg);
-			break;
-		case 'f':
-			conf_file = optarg;
-			break;
-		}
-	}	
+  while (-1 != (o = getopt(argc, argv, "N:f:"))) {
+    switch(o) {
+    case 'N':
+      core_limit = mystrtol(optarg, 10);
+      if (core_limit > num_cores) {
+        on_error("CPU limit should be smaller than the "
+               "number of CPUS: %d\n", num_cores);
+        return -1;
+      } else if (core_limit < 1) {
+        on_error("CPU limit should be greater than 0\n");
+        return -1;
+      }
+      /** 
+       * it is important that core limit is set 
+       * before mtcp_init() is called. You can
+       * not set core_limit after mtcp_init()
+       */
+      mtcp_getconf(&mcfg);
+      mcfg.num_cores = core_limit;
+      mtcp_setconf(&mcfg);
+      break;
+    case 'f':
+      conf_file = optarg;
+      break;
+    }
+  } 
 
-	if (conf_file == NULL) {
-		on_error("mTCP configuration file is not set!\n");
-		exit(-1);
-	}
-	
-	err = mtcp_init(conf_file);
-	if (err) {
-		on_error("Failed to initialize mtcp.\n");
-		exit(-1);
-	}
-	
+  if (conf_file == NULL) {
+    on_error("mTCP configuration file is not set!\n");
+    exit(-1);
+  }
+  
+  err = mtcp_init(conf_file);
+  if (err) {
+    on_error("Failed to initialize mtcp.\n");
+    exit(-1);
+  }
+  
   mtcp_core_affinitize(0);
   mctx = mtcp_create_context(0);
   server_fd = mtcp_socket(mctx, AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) on_error("Could not create socket\n");
+  if (server_fd < 0) 
+    on_error("Could not create socket\n");
 
   server.sin_family = AF_INET;
   server.sin_port = htons(ECHO_PORT);
   server.sin_addr.s_addr = htonl(INADDR_ANY);
 
   err = mtcp_bind(mctx, server_fd, (struct sockaddr *) &server, sizeof(server));
-  if (err < 0) on_error("Could not bind socket\n");
+  if (err < 0) 
+    on_error("Could not bind socket\n");
 
   err = mtcp_listen(mctx, server_fd, 128);
-  if (err < 0) on_error("Could not listen on socket\n");
+  if (err < 0) 
+    on_error("Could not listen on socket\n");
 
   printf("Server is listening on %d\n", ECHO_PORT);
 
   while (1) {
     socklen_t client_len = sizeof(client);
+    int read;
+    
     client_fd = mtcp_accept(mctx, server_fd, (struct sockaddr *) &client, &client_len);
 
-    if (client_fd < 0) on_error("Could not establish new connection\n");
+    if (client_fd < 0) 
+      on_error("Could not establish new connection\n");
 
-	mtcp_setsock_nonblock(mctx, client_fd);
+    mtcp_setsock_nonblock(mctx, client_fd);
     while (1) {
-      int read = mtcp_recv(mctx, client_fd, buf, BUFFER_SIZE, 0);
-	  if(read == 0) {
-		  on_error("Client read failed\n");
-		  break;
-	  } else {
-		  continue;
-	  }
+      read = mtcp_recv(mctx, client_fd, buf, BUFFER_SIZE, 0);
+      if(read == 0) {
+        on_error("Client read failed\n");
+        break;
+      } else {
+        continue;
+      }
     }
-	
-      err = mtcp_write(mctx, client_fd, buf, (size_t)read);
-      if (err < 0) on_error("Client write failed\n");	
+  
+    err = mtcp_write(mctx, client_fd, buf, (size_t)read);
+    if (err < 0) 
+      on_error("Client write failed\n"); 
   }
 
   return 0;
